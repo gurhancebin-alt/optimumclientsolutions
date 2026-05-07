7b7bc8f18a40b8eb46fc8ed936f150f869abc58c
gurhancebin-alt <gurhan.cebin@optimumclientsolutions.com>
2026-05-07 03:57:40 +0200

    Add MAINTENANCE_MODE constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const COMPANY_NAME = 'Optimum Client Solutions';
 export const CONTACT_EMAIL = 'gurhan.cebin@optimumclientsolutions.com';
 export const GA_MEASUREMENT_ID = 'G-FL0LQNWMRT';
+export const MAINTENANCE_MODE = true;
